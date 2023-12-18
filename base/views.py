278cda2aa345c04f5f7e7288dd874b2459d13240
Yavisth <yavisth.chopra@gmail.com>
2023-12-18 16:45:46 +0530

    BE_stg7_MediaAndABUser

--- base/views.py
+++ base/views.py
@@ -3,8 +3,8 @@ from django.http import HttpResponse
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
 from django.db.models import Q
-from .models import Room, Topic, Message
-from django.contrib.auth.models import User
+from .models import Room, Topic, Message, User
+# from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth import authenticate, login, logout
 from .forms import RoomForm, UserForm
